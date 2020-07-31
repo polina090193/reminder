6bb622c48c4f3912a40148ebd0bfb6153c9f980a
polina090193 <polina_0901@mail.ru>
2020-07-31 22:29:10 +0300

    Fix and a bit rewrite clock

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,38 @@
-function getDate () {
+function setDate () {
     let dateWrap = document.querySelector('.date');
     let timeWrap = document.querySelector('.time')
     let now = new Date();
-    
-    let currentDay = now.getDate();
-    let currentMonth = now.getMonth()
-    let currentYear = now.getFullYear();
-    let currentHours = now.getHours();
-    let currentMinutes = now.getMinutes();
-    let currentSeconds = now.getSeconds();
-    
-    dateWrap.innerHTML = addZeroIfNeeded(currentDay) + '.';
-    dateWrap.append (addZeroIfNeeded(currentMonth + 1) + '.');
-    dateWrap.append (addZeroIfNeeded(currentYear));
-    timeWrap.innerHTML = addZeroIfNeeded(currentHours) + ':';
-    timeWrap.append(addZeroIfNeeded(currentMinutes) + ':');
-    timeWrap.append(addZeroIfNeeded(currentSeconds));
 
-    function addZeroIfNeeded(num) {
+    let current = {
+        day: now.getDate(),
+        month: now.getMonth() + 1,
+        year: now.getFullYear(),
+        hours: now.getHours(),
+        minutes: now.getMinutes(),
+        seconds: now.getSeconds(),
+    }
+
+    for (let key in current) {
+        current[key] = addZeroIfNecessary(current[key])
+    }
+
+    function addZeroIfNecessary(num) {
         return num < 10 ? '0' + num : num;
     }
 
-    setTimeout(getDate)
+    function addDate (current) {
+        dateWrap.innerHTML = current.day + '.';
+        dateWrap.append(current.month + '.');
+        dateWrap.append(current.year);
+
+        timeWrap.innerHTML = current.hours + ':';
+        timeWrap.append(current.minutes + ':');
+        timeWrap.append(current.seconds);
+    }
+    
+    addDate(current)
+
+    setTimeout(setDate)
 }
-getDate()
+
+setDate()
