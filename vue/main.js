c31017c49837477d802f44a32a26fb19db963872
polina090193 <52452187+polina090193@users.noreply.github.com>
2021-04-14 19:06:15 +0300

    Update main.js

--- vue/main.js
+++ vue/main.js
@@ -25,7 +25,7 @@ let reminder = new Vue({
 
   data: {
 
-    reminders = remindersStorage.fetch(),
+    reminders: remindersStorage.fetch(),
 
     newReminder: {
       title: '',
@@ -36,15 +36,39 @@ let reminder = new Vue({
   },
 
   computed: {
-    /* listItem() {
-      return this.title + " " + this.date + " " + this.time;
-    }, */
+    
+    reminderItem() {
+      if (this.newReminder.date && this.newReminder.time) {
+
+        let dateObj = new Date(this.newReminder.date + 'T' + this.newReminder.time);
+  
+        let dd = dateObj.getDate();
+  
+        if (dd < 10) dd = '0' + dd;
+  
+        let mm = dateObj.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+  
+        let yy = dateObj.getFullYear() % 100;
+        if (yy < 10) yy = '0' + yy;
+  
+        let hh = dateObj.getHours();
+        if (hh < 10) hh = '0' + hh;
+  
+        let min = dateObj.getMinutes();
+        if (min < 10) min = '0' + min;
+  
+        return this.newReminder.title + ' ' + dd + '.' + mm + '.' + yy + ' ' + hh + ':' + min;
+
+      }
+    },
+
   },
 
   methods: {
 
     addReminder() {
-
+      alert('added!');
     },
 
     removeReminder(reminder) {
